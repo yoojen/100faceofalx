f414c3c38f14d935c459ea1fa9e3fe6d71ce89d1
Eugene <eugenemutuyimana15@gmail.com>
2024-03-17 22:26:27 +0200

    user registration and authentication done

---
+++ django_projects/django_blog/users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import redirect, render
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Your Account has been created! You can login')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         # form =
+#         pass
