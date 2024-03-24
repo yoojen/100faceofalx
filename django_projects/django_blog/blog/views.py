a55f64e3a4bbeebfeeb2d9bb9f5297e78f9b44a9
Eugene <eugenemutuyimana15@gmail.com>
2024-03-24 23:29:05 +0200

    added reset password urls

--- django_projects/django_blog/blog/views.py
+++ django_projects/django_blog/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,  get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
