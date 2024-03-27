694a84699cc27914e963574774a34dd4df69f8f2
Eugene <eugenemutuyimana15@gmail.com>
2024-03-27 17:52:57 +0200

    custom login

--- django_projects/django_movie/movie_app/movie_app/urls.py
+++ django_projects/django_movie/movie_app/movie_app/urls.py
@@ -14,16 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from users.views import CustomPasswordResetView
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, 
+                                       LogoutView,
+                                         PasswordResetDoneView,
+                                         PasswordResetConfirmView,
+                                         PasswordResetCompleteView)
 from django.conf import settings
 from django.conf.urls.static import static
+from users.views import CustomUserLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', CustomUserLoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', 
+         CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete',
+         PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('movie.urls')),
     path('', include('users.urls'))
 ]
