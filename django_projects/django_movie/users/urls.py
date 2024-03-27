4b63b6e261e26b3fd0335248837a544a6d76d26a
Eugene <eugenemutuyimana15@gmail.com>
2024-03-27 18:30:08 +0200

    removed

--- django_projects/django_movie/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import profile, register
-
-urlpatterns=[
-    path('register/', register, name='register'),
-    path('profile/', profile, name='profile'),
-]
