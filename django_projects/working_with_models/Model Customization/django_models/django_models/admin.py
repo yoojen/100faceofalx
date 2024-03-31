67236e55ced8eb933374360b6718a7e9a50edb7f
Eugene <eugenemutuyimana15@gmail.com>
2024-03-31 19:51:01 +0200

    registered models

---
+++ django_projects/working_with_models/Model Customization/django_models/django_models/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import President, PresidentInfo, City, Country
+admin.site.register(President)
+admin.site.register(PresidentInfo)
+admin.site.register(City)
+admin.site.register(Country)
