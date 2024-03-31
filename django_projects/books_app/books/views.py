24af830a1950b8ed5f15f42696dd4af0f9226d8b
Eugene <eugenemutuyimana15@gmail.com>
2024-03-31 23:05:48 +0200

    created custom mixin to check authorization

--- django_projects/books_app/books/views.py
+++ django_projects/books_app/books/views.py
@@ -1,10 +1,24 @@
-from django.contrib.auth.views import LoginView
-from django.views.generic import ListView, DetailView, DeleteView, UpdateView
-from django.http import HttpResponse
+from typing import Any
+from django.contrib.auth.views import LoginView, redirect_to_login
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from .forms import UserRegistrationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Book
+
+
+class BookAuthMixin(PermissionRequiredMixin):
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if not request.user.is_authenticated:
+            return redirect_to_login(self.request.get_full_path(),
+                                     self.get_login_url(),
+                                     self.get_redirect_field_name())
+
+        if not self.has_permission():
+            return redirect('books-home')
+        return super(BookAuthMixin, self).dispatch(request, *args, **kwargs)
 
 
 class CustomLoginView(LoginView):
@@ -13,6 +27,8 @@ class CustomLoginView(LoginView):
 
 
 def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('books-home')
     if request.method != 'POST':
         form = UserRegistrationForm()
     else:
@@ -24,11 +40,19 @@ def register_user(request):
     return render(request, 'books/register.html', {'form': form})
 
 
-def homeview(request):
-    return HttpResponse(f'HOMEPAGE, WELCOME BACK {request.user.username}')
+class CreateBookView(BookAuthMixin, CreateView):
+    raise_exception = False
+    permission_required = 'books.add_book'
+    redirect_field_name = 'next'
+    permission_denied_message = ''
+
+    model = Book
+    context_object_name = 'book'
+    fields = ['title', 'author']
 
 
-class UpdateBookView(UpdateView):
+class UpdateBookView(BookAuthMixin, UpdateView):
+    permission_required = 'books.change_book'
     model = Book
     context_object_name = 'book'
     fields = ['title', 'author']
