9ac8829b0bb31479c7d1fa59beaeb435e7267221
Eugene <eugenemutuyimana15@gmail.com>
2024-03-31 23:05:17 +0200

    added edit book url

--- django_projects/books_app/books/urls.py
+++ django_projects/books_app/books/urls.py
@@ -1,8 +1,11 @@
-from .views import BookListView, BookDetailView, BookDeleteView, UpdateBookView
+from .views import (BookListView, BookDetailView,
+                    BookDeleteView, UpdateBookView,
+                    CreateBookView)
 from django.urls import path
 urlpatterns = [
     path('', BookListView.as_view(), name='books-home'),
     path('book/<int:pk>/', BookDetailView.as_view(), name='book-view'),
+    path('book/add_new', CreateBookView.as_view(), name='book-add'),
     path('book/<int:pk>/edit/', UpdateBookView.as_view(), name='book-update'),
     path('book/<int:pk>/delete/', BookDeleteView.as_view(),  name='book-delete')
 ]
