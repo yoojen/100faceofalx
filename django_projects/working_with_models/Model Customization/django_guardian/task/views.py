24d8c4420fe499110c72f850001d17bdea3c63b6
Eugene <eugenemutuyimana15@gmail.com>
2024-04-02 16:51:45 +0200

    added dashboard view with new guardian queryset

--- django_projects/working_with_models/Model Customization/django_guardian/task/views.py
+++ django_projects/working_with_models/Model Customization/django_guardian/task/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from guardian.shortcuts import get_objects_for_user
 
-# Create your views here.
+
+def user_dashboard(request, ):
+    tasks = get_objects_for_user(request.user, 'task.view_task')
+    return render(request, 'task/dashboard.html', {'tasks': tasks})
