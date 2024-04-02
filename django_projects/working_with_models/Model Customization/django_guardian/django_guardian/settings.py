135f80ed66870511e01266117d13c305e19d46ff
Eugene <eugenemutuyimana15@gmail.com>
2024-04-02 15:43:29 +0200

    added anonymous user name

--- django_projects/working_with_models/Model Customization/django_guardian/django_guardian/settings.py
+++ django_projects/working_with_models/Model Customization/django_guardian/django_guardian/settings.py
@@ -129,3 +129,5 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # this is default
     'guardian.backends.ObjectPermissionBackend',
 )
+
+ANONYMOUS_USER_NAME = 'AnonymousUser'
