935c62009e301486b428397d02dd9cff2421efcb
Eugene <eugenemutuyimana15@gmail.com>
2024-04-02 15:04:31 +0200

    created profile models

--- django_projects/working_with_models/Model Customization/django_guardian/profiles/models.py
+++ django_projects/working_with_models/Model Customization/django_guardian/profiles/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from datetime import date
 
 
 class Profile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bod = models.DateField(default=date(2002, 1, 1))
