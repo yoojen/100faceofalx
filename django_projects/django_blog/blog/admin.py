124b4d147e46ca3c9a5d7dbe504d73115900a555
Eugene <eugenemutuyimana15@gmail.com>
2024-04-05 15:47:31 +0200

    created PostAdmin with new action

--- django_projects/django_blog/blog/admin.py
+++ django_projects/django_blog/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
-# Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    search_fields = ['title']
+    actions = ['make_post_inactive']
+
+    @admin.action(description="Set post inactive", permissions=['change'])
+    def make_post_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+
+
+admin.site.register(Post, PostAdmin)
