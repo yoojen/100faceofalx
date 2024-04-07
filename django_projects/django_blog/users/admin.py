86e90e84b3ba1eed3830b55f5c814f416d751280
Eugene <eugenemutuyimana15@gmail.com>
2024-04-07 23:07:37 +0200

    added action on admin

--- django_projects/django_blog/users/admin.py
+++ django_projects/django_blog/users/admin.py
@@ -1,10 +1,11 @@
+from typing import Any
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Profile
 
 
-class ProfileInline(admin.StackedInline):
+class ProfileInline(admin.TabularInline):
     model = Profile
     verbose_name_plural = "user profile"
     can_delete = False
