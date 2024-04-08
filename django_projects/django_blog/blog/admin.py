536af221e5ce683fe9b3a7ad5722a92a4b56cc31
Eugene <eugenemutuyimana15@gmail.com>
2024-04-08 17:26:48 +0200

    added guardian objects permission

--- django_projects/django_blog/blog/admin.py
+++ django_projects/django_blog/blog/admin.py
@@ -1,57 +1,57 @@
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import Post
+from guardian.shortcuts import get_objects_for_user
+from guardian.admin import GuardedModelAdmin
 
-class CustomPostMixin:
-
-
-    # def has_module_permission(self, request):
-    #     if super().has_module_permission(request):
-    #         return True
-    
-    # def get_queryset(self, request):
-    #     return super().get_queryset(request)
-    
-    # def has_permission(self, request):
-    #     return super().has_module_permission(request)
-    
-    def has_view_permission(self, request, obj=None):
-        if request.user.is_superuser:
-            return True
-        if request.user.has_perm("blog.view_post"):
-            return True
-
-    def has_add_permission(self, request, obj=None):
-        if request.user.is_superuser:
-            return True
-        if request.user.has_perm("blog.add_post"):
-            return True
-
-    def has_change_permission(self, request, obj=None):
-        if request.user.is_superuser:
-            return True
-        if request.user.has_perm("blog.change_post"):
-            return True
-
-    def has_delete_permission(self, request, obj=None):
-        if request.user.is_superuser:
-            return True
-        if request.user.has_perm("blog.delete_post"):
-            return True
-
-
-class PostAdmin(CustomPostMixin, admin.ModelAdmin):
+class PostAdmin(GuardedModelAdmin, admin.ModelAdmin):
     list_per_page = 10
-    search_fields = ['title']
-    actions = ['make_post_inactive']
+    search_fields = ["title"]
+    actions = ["make_post_inactive"]
 
-    # def get_form(self, request, obj, **kwargs):
-    #     form = super().get_form(request, obj, **kwargs)
-    #     super_user = request.user.is_superuser
-    #     if not super_user:
-    #         form.base_fields['author'].disabled = True
-    #     return form
 
-    @admin.action(description="Set post inactive", permissions=['change'])
+    def has_module_permission(self, request):
+        if super().has_module_permission(request):
+            return True
+        return self.get_model_object(request).exists()
+        
+
+    def get_queryset(self, request):
+        opts = self.opts
+        if request.user.is_superuser:
+            return super().get_queryset(request)
+        return self.get_model_object(request)
+       
+    def get_model_object(self, request, actions=None, klass=None):
+        opts = self.opts
+        klass = klass if klass else opts.model
+        actions = [actions] if actions else ['view', 'add', 'delete', 'change']
+        model_name = klass._meta.model_name
+        return get_objects_for_user(user=request.user,
+                             perms=[f"{perm}_{model_name}" for perm in actions],
+                             any_perm=True, klass=klass)
+
+    def has_permission(self, request, obj, action):
+        opts = self.opts
+        perm=f"{opts.app_label}.{action}_{opts.model_name}"
+        if obj:
+            return request.user.has_perm(perm, obj)
+        else:
+            return True
+        
+    def has_view_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'view')
+    
+    def has_change_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'change')
+    
+    def has_delete_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'delete')
+    
+    def has_add_permission(self, request, obj=None):
+        return self.has_permission(request, obj, 'add')
+    
+    @admin.action(description="Set post inactive", permissions=["change"])
     def make_post_inactive(self, request, queryset):
         queryset.update(is_active=False)
 
