ffbfb8520a325b45e1822eefbe7c6220d4b60ec0
Eugene <eugenemutuyimana15@gmail.com>
2024-04-08 17:27:09 +0200

    changed admin inlines

--- django_projects/books_app/books/admin.py
+++ django_projects/books_app/books/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Book, Author
-# Register your models here.
-admin.site.register([Book, Author])
+from django.contrib.auth.models import User
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    can_delete = True
+    verbose_name_plural = 'author'
+
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ("Advanced Options", {
+            "classes": ["collapse"],
+            "fields": ["author"]
+        })
+    ]
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
