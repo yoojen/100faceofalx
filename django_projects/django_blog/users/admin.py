0e2f1bb2d992ff1bc08f98d54f05070010602082
Eugene <eugenemutuyimana15@gmail.com>
2024-04-11 23:50:39 +0200

    harnesed django-permissions

--- django_projects/django_blog/users/admin.py
+++ django_projects/django_blog/users/admin.py
@@ -1,9 +1,9 @@
-from typing import Any
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.http import HttpRequest
 from .models import Profile
-
+from guardian.admin import GuardedModelAdmin
 
 class ProfileInline(admin.TabularInline):
     model = Profile
@@ -16,12 +16,25 @@ class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline]
 
 
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(GuardedModelAdmin, admin.ModelAdmin):
     list_display = ['user', 'image']
     search_fields = ['user__exact']
     list_per_page = 10
     actions = ["update_image"]
 
+
+    def has_view_permission(self, request, obj=None):
+        return super().has_view_permission(request, obj)
+    
+    def has_change_permission(self, request, obj=None):
+        return super().has_change_permission(request, obj)
+    
+    def has_add_permission(self, request):
+        return super().has_add_permission(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return super().has_delete_permission(request, obj)
+    
     @admin.action(description="Set image from admin page", permissions=['change'])
     def update_image(self, request, queryset):
         queryset.update(image="profile_pics/IMG_20201126_083637_9.jpg")
