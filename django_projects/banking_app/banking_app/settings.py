e394e422ca17bc50391b6ddda5a8486f3f4ca477
Eugene <eugenemutuyimana15@gmail.com>
2024-04-12 23:22:26 +0200

    main setting

--- django_projects/banking_app/banking_app/settings.py
+++ django_projects/banking_app/banking_app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transactions',
+    'profiles',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'guardian',
 ]
 
 MIDDLEWARE = [
