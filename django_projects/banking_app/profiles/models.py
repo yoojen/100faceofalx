8f8adda23451be2c293d2724da63f37289f114a2
Eugene <eugenemutuyimana15@gmail.com>
2024-04-12 23:22:10 +0200

    customer models done

--- django_projects/banking_app/profiles/models.py
+++ django_projects/banking_app/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomerProfile(models.Model):
+    customer = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField(null=False)
+    tel = models.CharField(max_length=250, null=False)
+    province=models.CharField(max_length=250, null=False)
+    district=models.CharField(max_length=250, null=False)
+    sector=models.CharField(max_length=250, null=False)
+    cell=models.CharField(max_length=250, null=False)
