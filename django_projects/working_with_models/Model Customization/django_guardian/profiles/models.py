67c584fa37e63b7926ed5459a5409524b7df0445
Eugene <eugenemutuyimana15@gmail.com>
2024-04-12 22:09:51 +0200

    'deleted'

--- django_projects/working_with_models/Model Customization/django_guardian/profiles/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from datetime import date
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bod = models.DateField(default=date(2002, 1, 1))
--- django_projects/working_with_models/Model Customization/django_guardian/task/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from guardian.shortcuts import get_objects_for_user
-
-
-def user_dashboard(request):
-    tasks = get_objects_for_user(
-        request.user, 'task.view_task', accept_global_perms=False)
-    return render(request, 'task/dashboard.html', {'tasks': tasks})
