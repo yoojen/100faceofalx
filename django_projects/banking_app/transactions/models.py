60954b4820997a9a48df0cf6a14c86e50e80e8d2
Eugene <eugenemutuyimana15@gmail.com>
2024-04-13 16:24:00 +0200

    added new models

--- django_projects/banking_app/transactions/models.py
+++ django_projects/banking_app/transactions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date, timedelta
 
-# Create your models here.
+class Account(models.Model):
+    customer = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(default=0.00)
+    opening_date = models.DateField(auto_now=True)
+
+class Transactions(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.DecimalField()
+    date_done = models.DateField(auto_now=True)
+    description = models.CharField(max_length=250, null=True)
+
+class Card(models.Model):
+    customer = models.OneToOneField(User, on_delete=models.CASCADE)
+    card_type = models.CharField(max_length=250)
+    card_number = models.CharField(max_length=250)
+    expiration_date = models.DateField(default=date.today() + timedelta(365))
