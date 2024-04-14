9cd687735e8cd3139f94bbcf9b547c520612eb99
Eugene <eugenemutuyimana15@gmail.com>
2024-04-14 16:45:16 +0200

    inlines added

--- django_projects/banking_app/profiles/admin.py
+++ django_projects/banking_app/profiles/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import CustomerProfile, User
-from django.contrib.auth.admin import UserAdmin
+
+
+class CustomerProfileInline(admin.StackedInline):
+    model = CustomerProfile
+    can_delete = True
+    verbose_name_plural = "customer profile"
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    list_per_page = 10
+
+
+    inlines = [CustomerProfileInline]
+
+   
+    # @admin.action("Set user inactive")
+
 
 admin.site.register(CustomerProfile)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
