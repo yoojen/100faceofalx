9d1b2ac6d439c846b040c952d73b872d80199e50
Eugene <eugenemutuyimana15@gmail.com>
2024-04-14 15:03:00 +0200

    defined user model

--- django_projects/banking_app/profiles/models.py
+++ django_projects/banking_app/profiles/models.py
@@ -1,6 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser 
+from .managers import CustomUserManager
 
+
+class User(AbstractUser):
+    username=None
+    email = models.EmailField(unique=True,blank=True, null=True)
+    type = models.CharField(max_length=250, default="CUSTOMER")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+    class Meta:
+        permissions = [
+            ("can_control_manager", "Can add, change or delete manager"),
+            ("can_control_cashier", "Can add, change or delete"),
+        ]
 
 class CustomerProfile(models.Model):
     customer = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,4 +26,4 @@ class CustomerProfile(models.Model):
     district=models.CharField(max_length=250, null=False)
     sector=models.CharField(max_length=250, null=False)
     cell=models.CharField(max_length=250, null=False)
-    image = models.ImageField(default='Contact.png', upload_to='/media/profile_pictures')
+    image = models.ImageField(default='Contact.png', upload_to='../media/profile_pictures')
