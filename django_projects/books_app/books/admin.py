6dba9f84a400f0d77a43f1ac6affc5f9f9f9e90e
Eugene <eugenemutuyimana15@gmail.com>
2024-04-14 16:46:23 +0200

    checking for admin

--- django_projects/books_app/books/admin.py
+++ django_projects/books_app/books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Book, Author
 from django.contrib.auth.models import User
 
