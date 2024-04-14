4d23c678713e236759eaf38d6cf539fe7bf74ee3
Eugene <eugenemutuyimana15@gmail.com>
2024-04-14 16:46:01 +0200

    added filters

--- django_projects/banking_app/transactions/admin.py
+++ django_projects/banking_app/transactions/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
 from .models import (Account,
-                    Transactions,
-                    Card,
-                    BillInfo
-                    )
-admin.site.register([Account, Transactions, Card, BillInfo])
+                     Transactions,
+                     Card,
+                     BillInfo
+                     )
+from profiles.admin import CustomerProfileInline
+
+
+class AccountAdmin(admin.ModelAdmin):
+    search_fields = ["account_num__exact"]
+    list_per_page = 10
+    list_display = ["account_num", "customer", "balance", "date_opened"]
+
+
+class TransactionsAdmin(admin.ModelAdmin):
+    search_fields = ["account__exact"]
+    list_display = ["account", "amount", "date_done", "description"]
+    list_per_page = 10
+
+
+class CardAdmin(admin.ModelAdmin):
+    search_fields = ["card_number__exact"]
+    list_display = ["customer", "card_type", "card_number",
+                    "expiration_date", "is_active", "issue_date"]
+    list_per_page = 10
+
+
+class BillInfoAdmin(admin.ModelAdmin):
+    search_fields = ["payee_account__exact"]
+    list_display = ["customer", "payee_name",
+                    "payee_account", "payment_date"]
+    list_per_page = 10
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Transactions, TransactionsAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(BillInfo, BillInfoAdmin)
