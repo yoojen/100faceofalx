8c162bb343af877d31b37035226c52089edae49c
Eugene <eugenemutuyimana15@gmail.com>
2024-04-14 15:02:25 +0200

    unmarked external urls

--- django_projects/banking_app/banking_app/urls.py
+++ django_projects/banking_app/banking_app/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('profiles.urls')),
-    path('', include('transactions.urls'))
+    # path('', include('profiles.urls')),
+    # path('', include('transactions.urls'))
 ]
