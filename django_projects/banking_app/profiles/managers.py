84e40dd11e6ba4ca7084135cb47920aa9503746e
Eugene <eugenemutuyimana15@gmail.com>
2024-04-15 23:51:44 +0200

    creation of custom user manager

--- django_projects/banking_app/profiles/managers.py
+++ django_projects/banking_app/profiles/managers.py
@@ -3,7 +3,6 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
 
-
     def create_user(self, email, password, **extra_fields):
         """
         Create and save new user with given attributes
@@ -11,8 +10,7 @@ class CustomUserManager(BaseUserManager):
 
         if not email:
             raise ValueError("Please provide email")
-        
-        
+
         extra_fields.setdefault("is_active", True)
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
@@ -21,19 +19,19 @@ class CustomUserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email=None, password=None, **extra_fields):
         """
         Create super user with is_superuser and is_staff enabled
         """
-        
+
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_staff", True)
-        
+
         if not extra_fields.get("is_superuser"):
             raise ValueError("is_superuser has to be to true")
         if not extra_fields.get("is_staff"):
             raise ValueError("is_staff has to be set to true")
         user = self.create_user(email, password, **extra_fields)
-        user.type='ADMIN'
+        user.type = 'ADMIN'
         user.save()
         return user
