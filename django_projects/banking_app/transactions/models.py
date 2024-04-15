ab5fd52d2db2ee705d57086859f8b126166e601c
Eugene <eugenemutuyimana15@gmail.com>
2024-04-15 23:52:15 +0200

    changed account model

--- django_projects/banking_app/transactions/models.py
+++ django_projects/banking_app/transactions/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from profiles.models import User
 from datetime import date, timedelta
 
+
 class Account(models.Model):
-    account_num = models.CharField(max_length=13, unique=True, blank=False, null=False)
+    account_num = models.CharField(
+        max_length=13, unique=True, blank=False, null=False)
     customer = models.OneToOneField(User, on_delete=models.CASCADE)
-    balance = models.DecimalField(default=0.00,decimal_places=2, max_digits=10)
+    balance = models.DecimalField(
+        default=0.00, decimal_places=2, max_digits=10)
     date_opened = models.DateField(auto_now=True)
 
     def __str__(self) -> str:
-        return f"{self.customer.email}"
+        return f"{self.account_num}"
+
 
 class Transactions(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -19,6 +23,7 @@ class Transactions(models.Model):
 
     def __str__(self) -> str:
         return f"{self.account.account_num} for {self.account.customer.email}"
+
 
 class Card(models.Model):
     customer = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -31,13 +36,13 @@ class Card(models.Model):
     def __str__(self) -> str:
         return f"{self.customer.email}"
 
+
 class BillInfo(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     payee_name = models.CharField(max_length=250)
-    payee_account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
-    payment_date= models.DateField(auto_now=True)
-
+    payee_account = models.ForeignKey(
+        Account, on_delete=models.SET_NULL, null=True)
+    payment_date = models.DateField(auto_now=True)
 
     def __str__(self) -> str:
         return f"{self.customer.email}"
-    
