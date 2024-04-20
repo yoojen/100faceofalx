f511a224ded2457a628c83a9ef5764f9ed866742
Eugene <eugenemutuyimana15@gmail.com>
2024-04-20 20:52:04 +0200

    refactored code

--- django_projects/banking_app/profiles/admin.py
+++ django_projects/banking_app/profiles/admin.py
@@ -8,31 +8,7 @@ from guardian.shortcuts import get_objects_for_user
 from .forms import CustomUserChangeForm
 
 
-class CustomerProfileInline(admin.StackedInline):
-    model = CustomerProfile
-    can_delete = True
-    verbose_name_plural = "customer profile"
-
-
-@admin.register(User)
-class UserAdmin(GuardedModelAdmin,  BaseUserAdmin):
-    model = User
-    form = CustomUserChangeForm
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide"),
-            "fields": (
-                "username", "email", "password1", "password2", "first_name", "last_name",
-                "is_staff",  "is_active", "is_superuser", "groups", "user_permissions"
-            )
-        }),
-    )
-
-    def obj_perms_manage_user_view(self, request, object_pk, user_id):
-        if not request.user.has_perm('guardian.change_userobjectpermission'):
-            return redirect('admin/')
-        return super().obj_perms_manage_user_view(request, object_pk, user_id)
-
+class CustomObjectAccessMixin(GuardedModelAdmin):
     def has_module_permission(self, request):
         if super().has_module_permission(request):
             return True
@@ -46,7 +22,9 @@ class UserAdmin(GuardedModelAdmin,  BaseUserAdmin):
             f"{self.opts.app_label}.{perm}_{self.opts.model_name}" for perm in actions]
         data = get_objects_for_user(
             user=request.user, perms=perms, klass=klass, any_perm=True)
-        return data if request.user.is_superuser else data.exclude(username='mreugene')
+        if klass is User and not request.user.is_superuser:
+            return data.exclude(is_superuser=True)
+        return data
 
     def has_permission(self, request, obj=None, action=None):
         opts = self.opts
@@ -55,15 +33,6 @@ class UserAdmin(GuardedModelAdmin,  BaseUserAdmin):
         if obj:
             return request.user.has_perm(f"{app_lebel}.{action}_{model_name}", obj)
         return request.user.has_perm(f"{app_lebel}.{action}_{model_name}")
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if self.has_permission(request, action="change") and not request.user.is_superuser:
-            form.base_fields["is_superuser"].disabled = True
-            form.base_fields["is_staff"].disabled = True
-            form.base_fields["groups"].disabled = True
-            form.base_fields["user_permissions"].disabled = True
-        return form
 
     def get_queryset(self, request, ):
         if request.user.is_superuser:
@@ -81,14 +50,49 @@ class UserAdmin(GuardedModelAdmin,  BaseUserAdmin):
         return self.has_permission(request, action="add")
 
     def has_change_permission(self, request, obj=None):
-        if request.user.is_staff:
-            return True
-        return self.has_permission(request, obj, "change")
+        if request.user.is_staff and obj != request.user:
+            return self.has_permission(request, action="change")
+        return self.has_permission(request, obj, action="change")
 
     def has_delete_permission(self, request, obj=None):
-        if request.user.is_staff:
-            return True
-        return self.has_permission(request, obj, "delete")
+        return self.has_permission(request, obj, action="delete")
+
+
+class CustomerProfileInline(admin.StackedInline):
+    model = CustomerProfile
+    can_delete = True
+    verbose_name_plural = "customer profile"
+
+
+@admin.register(User)
+class UserAdmin(CustomObjectAccessMixin,  BaseUserAdmin):
+    model = User
+    form = CustomUserChangeForm
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide"),
+            "fields": (
+                "username", "email", "password1", "password2", "first_name", "last_name",
+                "is_staff",  "is_active", "is_superuser", "groups", "user_permissions"
+            )
+        }),
+    )
+
+    def obj_perms_manage_user_view(self, request, object_pk, user_id):
+        if not request.user.has_perm('guardian.change_userobjectpermission'):
+            return redirect('admin/')
+        return super().obj_perms_manage_user_view(request, object_pk, user_id)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if self.has_permission(request, obj, action="change") and\
+                not request.user.is_superuser:
+            form.base_fields["is_superuser"].disabled = True
+            form.base_fields["is_staff"].disabled = True
+            form.base_fields["groups"].disabled = True
+            form.base_fields["user_permissions"].disabled = True
+            return form
+        return form
 
 
 admin.site.register(CustomerProfile)
