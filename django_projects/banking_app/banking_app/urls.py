6aedb15eacd3846011f4574c319631e6edd6f507
Eugene <eugenemutuyimana15@gmail.com>
2024-04-21 16:02:32 +0200

    included profile namespace

--- django_projects/banking_app/banking_app/urls.py
+++ django_projects/banking_app/banking_app/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', include('profiles.urls')),
+    path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
     path('transactions/', include(('transactions.urls', 'transactions'), namespace='transactions'))
 ]
