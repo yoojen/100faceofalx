c6a564278b5c9090934fb2ffdd796ff204fc62d2
Eugene <eugenemutuyimana15@gmail.com>
2024-04-25 23:55:08 +0200

    added new endpoint

--- django_projects/banking_app/transactions/urls.py
+++ django_projects/banking_app/transactions/urls.py
@@ -3,12 +3,14 @@ from .views import (TransactionPostView,
                      find_account,
                        AccountListView, 
                     my_combined_view,
-                       CustomerTransactionListView)
+                    CustomerTransactionListView,
+                      CreateAccountView)
 
 urlpatterns = [
     path('transact/', TransactionPostView.as_view(), name="transact"),
     path('find_account/', find_account, name="find_account"),
-    path('acc_inspect/', AccountListView.as_view(), name="acc_inspect"),
-    path('acc_inspect/<int:pk>', my_combined_view,
-         name="single_acc_insept"),
+    path('acc-inspect/', AccountListView.as_view(), name="acc_inspect"),
+    path('acc-inspect/<int:pk>', my_combined_view,
+         name="single_acc_inspect"),
+    path('create-account/', CreateAccountView.as_view(), name="create_account")
 ]
