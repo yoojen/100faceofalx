4793062c2869d3fea5afb9448e76a3ebe302de07
Eugene <eugenemutuyimana15@gmail.com>
2024-04-25 23:59:00 +0200

    changed url

--- django_projects/banking_app/profiles/urls.py
+++ django_projects/banking_app/profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profiles
+from .views import profiles, CreateUserView
 
 urlpatterns = [
+    path('create-user', CreateUserView.as_view(), name="create_user"),
     path('profiles/', profiles, name="profile")
 ]
