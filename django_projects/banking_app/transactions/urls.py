df77a6ec469b6080de29825ff9fbcebc03843a28
Eugene <eugenemutuyimana15@gmail.com>
2024-04-29 17:56:14 +0200

    added new urls conf

--- django_projects/banking_app/transactions/urls.py
+++ django_projects/banking_app/transactions/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from .views import (TransactionPostView,
-                     find_account,
-                       AccountListView, 
-                    my_combined_view,
+                    AccountListView, 
                     CustomerTransactionListView,
-                      CreateAccountView)
+                    CreateAccountView,
+                    find_account,
+                    my_combined_view,
+                    generate_account_number)
 
 urlpatterns = [
     path('transact/', TransactionPostView.as_view(), name="transact"),
-    path('find_account/', find_account, name="find_account"),
     path('acc-inspect/', AccountListView.as_view(), name="acc_inspect"),
     path('acc-inspect/<int:pk>', my_combined_view,
          name="single_acc_inspect"),
-    path('create-account/', CreateAccountView.as_view(), name="create_account")
+    path('create-account/', CreateAccountView.as_view(), name="create_account"),
+    path('find-account/', find_account, name="find_account"),
+    path('gen-acc/', generate_account_number, name="gen_acc"),
 ]
