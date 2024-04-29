5ce357dda369019c2d80e8ceff521b398478ca6c
Eugene <eugenemutuyimana15@gmail.com>
2024-04-29 17:53:32 +0200

    added new url confs

--- django_projects/banking_app/banking_app/urls.py
+++ django_projects/banking_app/banking_app/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
     path('transactions/', include(('transactions.urls', 'transactions'), namespace='transactions'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
