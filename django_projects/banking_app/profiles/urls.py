18ae1a11f7dd44a0120a8adcd382f35e90d28522
Eugene <eugenemutuyimana15@gmail.com>
2024-04-29 17:57:22 +0200

    changed profiles url and views conf

--- django_projects/banking_app/profiles/urls.py
+++ django_projects/banking_app/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import profiles, CreateUserView
+from .views import profiles, CreateUserView, CreateUserProfileView
 
 urlpatterns = [
-    path('create-user', CreateUserView.as_view(), name="create_user"),
+    path('create-user/', CreateUserView.as_view(), name="create_user"),
+    path('create-user-profile/', CreateUserProfileView.as_view(), name="create_user_profile"),
     path('profiles/', profiles, name="profile")
 ]
--- django_projects/banking_app/profiles/views.py
+++ django_projects/banking_app/profiles/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, redirect
+from .models import User, CustomerProfile
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 
@@ -8,15 +8,34 @@ from .forms import CustomUserCreationForm
 class CreateUserView(CreateView):
     model = User
     form_class = CustomUserCreationForm
-    success_url = "create_account"
+    success_url = "create_user"
     
     def form_valid(self, form):
         super().form_valid(form)   
 
         if form.is_valid():
-            messages.success(request=self.request, message="Customer created successfully")
-        messages.error(request=self.request, message="Customer is not created")
-        return super().form_invalid()
+            messages.success(self.request, "Customer created successfully",
+                             f"User created with this email: {form.cleaned_data['email']}")
+            return redirect("profiles:create_user")
+        else:
+            messages.error(request=self.request, message="Customer is not created")
+            return super().form_invalid(form)
+        
+
+class CreateUserProfileView(CreateView):
+    model = CustomerProfile
+    fields = "__all__"
+    success_url = "create_user_profile"
+    
+    def form_valid(self, form):
+        super().form_valid(form)   
+
+        if form.is_valid():
+            messages.success(request=self.request, message="Customer profile created successfully")
+            return redirect("profiles:create_user_profile")
+        else:
+            messages.error(request=self.request, message="Customer profile is not created")
+            return super().form_invalid(form)
 
 def profiles(request):
     return render(request, template_name="profiles/base.html")
