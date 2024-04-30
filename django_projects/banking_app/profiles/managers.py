5a2ad6babcc8fd6cb387d4471633ea9028c83b37
Eugene <eugenemutuyimana15@gmail.com>
2024-04-30 17:21:11 +0200

    user used tel instead of email;

--- django_projects/banking_app/profiles/managers.py
+++ django_projects/banking_app/profiles/managers.py
@@ -4,25 +4,24 @@ from django.db.models.query import QuerySet
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, telephone, password, **extra_fields):
         """
         Create and save new user with given attributes
         """
 
-        if not email:
-            raise ValueError("Please provide email")
+        if not telephone:
+            raise ValueError("Please provide Phone number")
         if not password:
             raise ValueError("Provide password")
         extra_fields.setdefault("is_active", True)
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(telephone=telephone, **extra_fields)
         user.set_password(password)
         user.type="CUSTOMER"
-        user.save(using=self.db)
+        user.save()
 
         return user
 
-    def create_superuser(self, email=None, password=None, **extra_fields):
+    def create_superuser(self, telephone=None, password=None, **extra_fields):
         """
         Create super user with is_superuser and is_staff enabled
         """
@@ -34,7 +33,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError("is_superuser has to be to true")
         if not extra_fields.get("is_staff"):
             raise ValueError("is_staff has to be set to true")
-        user = self.create_user(email, password, **extra_fields)
+        user = self.create_user(telephone, password, **extra_fields)
         user.type = 'ADMIN'
-        user.save(using=self.db)
+        user.save()
         return user
