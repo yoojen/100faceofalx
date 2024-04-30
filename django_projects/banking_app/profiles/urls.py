862897d9399365ce37ffce3f8482777221f9dcd6
Eugene <eugenemutuyimana15@gmail.com>
2024-04-30 17:19:24 +0200

    created new view and urls

--- django_projects/banking_app/profiles/urls.py
+++ django_projects/banking_app/profiles/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import profiles, CreateUserView, CreateUserProfileView
+from .views import (CreateUserView, 
+                    CreateUserProfileView,
+                    CustomerListView, 
+                    CustomerDetailView,
+                    my_combined_view)
 
 urlpatterns = [
+    path('customers/', CustomerListView.as_view(), name="customers"),
+    path('customers/<int:pk>', my_combined_view, name="customer"),
     path('create-user/', CreateUserView.as_view(), name="create_user"),
     path('create-user-profile/', CreateUserProfileView.as_view(), name="create_user_profile"),
-    path('profiles/', profiles, name="profile")
 ]
--- django_projects/banking_app/profiles/views.py
+++ django_projects/banking_app/profiles/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+
+from banking_app.serializer import Serializer
+from transactions.models import Account, Transactions
 from .models import User, CustomerProfile
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 from .forms import CustomUserCreationForm
 
 
@@ -11,11 +14,11 @@ class CreateUserView(CreateView):
     success_url = "create_user"
     
     def form_valid(self, form):
-        super().form_valid(form)   
 
         if form.is_valid():
             messages.success(self.request, "Customer created successfully",
-                             f"User created with this email: {form.cleaned_data['email']}")
+                             f"User created for this phone number: {form.cleaned_data['telephone']}")
+            super().form_valid(form)   
             return redirect("profiles:create_user")
         else:
             messages.error(request=self.request, message="Customer is not created")
@@ -24,18 +27,55 @@ class CreateUserView(CreateView):
 
 class CreateUserProfileView(CreateView):
     model = CustomerProfile
-    fields = "__all__"
+    fields = ["telephone","dob","province", "district", "sector","cell","image"]
     success_url = "create_user_profile"
     
     def form_valid(self, form):
-        super().form_valid(form)   
-
-        if form.is_valid():
+        customer = User.objects.filter(
+            telephone=form.cleaned_data["telephone"]).first()
+        
+        try:
+            self.object = form.save(commit=False)
+            self.object.customer_id = Serializer.dumps(customer)["id"]
             messages.success(request=self.request, message="Customer profile created successfully")
+            super().form_valid(form)
             return redirect("profiles:create_user_profile")
-        else:
+        except Exception:
             messages.error(request=self.request, message="Customer profile is not created")
             return super().form_invalid(form)
 
-def profiles(request):
-    return render(request, template_name="profiles/base.html")
+
+class CustomerListView(ListView):
+    model = User
+    context_object_name = "customers"
+
+
+class CustomerDetailView(DetailView):
+    model = User
+
+
+class AccountDetailView(DetailView):
+    model = Account
+    template_name = "profiles/user_detail.html"
+
+
+class CustomerTransactionListView(ListView):
+    model = Transactions
+    template_name = "profiles/user_detail.html"
+
+
+def my_combined_view(request, pk=None):
+    list_data = []
+    customer = User.objects.filter(pk=pk).first()
+    account = Account.objects.filter(customer_phone_number=customer.telephone).first()
+    if account:
+        list_data = Transactions.objects.filter(
+            account_num=account.account_num).all()
+
+    context = {
+        "customer": customer,
+        'account': account if account else None,  # For DetailView
+        'list_data': list_data if list_data else None,          # For ListView
+    }
+    return render(request, 'profiles/user_detail.html', context)
+
