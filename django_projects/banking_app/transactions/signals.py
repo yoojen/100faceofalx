28f16e75580371f1c8cb01a170d00b23e3b3a3b3
Eugene <eugenemutuyimana15@gmail.com>
2024-05-01 19:05:21 +0200

    signal for transaction creation when account creted

---
+++ django_projects/banking_app/transactions/signals.py
@@ -0,0 +1,11 @@
+# create transaction when user is created with balance account
+from django.db.models.signals import post_save
+from .models import Transactions, Account
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=Account)
+def create_transaction(sender, instance, created, **kwargs):
+    if created:
+        Transactions.objects.create(account=instance, account_num=instance.account_num,
+                                    amount=instance.balance, type="Deposit")
