dcf6ee50bf0e7650f6c1b7389ac33bd0029a81a7
Eugene <eugenemutuyimana15@gmail.com>
2024-05-03 18:12:24 +0200

    bills creation view

--- django_projects/banking_app/transactions/views.py
+++ django_projects/banking_app/transactions/views.py
@@ -1,10 +1,13 @@
-from django.db.models  import Q
+from django.db.models import Q
+from .forms import BillForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Transactions, Account
+from django.urls import resolve
+from .models import Transactions, Account, BillInfo
 from django.views.generic import ListView, CreateView
 from banking_app.serializer import Serializer
 from profiles.models import CustomerProfile
+
 
 class TransactionPostView(CreateView):
     model = Transactions
@@ -35,23 +38,23 @@ class TransactionPostView(CreateView):
                 else:
                     account.balance = account.balance - amount
                     account.save()
-                    messages.success(self.request, self.success_message, f"Balance: {self.object.account.balance}")
+                    messages.success(
+                        self.request, self.success_message, f"Balance: {self.object.account.balance}")
             return super().form_valid(form)
         except Exception as e:
             messages.error(self.request, "Can't process the request", str(e))
             return super().form_invalid(form)
-        
 
 
 def find_account(request):
     account = Account.objects.filter(
         account_num=request.GET.get("account_num")).first()
     if account:
-        messages.success(request, "Account number found", Serializer.dumps(account))
+        messages.success(request, "Account number found",
+                         Serializer.dumps(account))
         return redirect("transactions:transact")
     messages.error(request, "Nothing found")
     return redirect("transactions:transact")
-
 
 
 class AccountListView(ListView):
@@ -71,7 +74,8 @@ def my_combined_view(request, pk=None):
     list_data = []
     detail_object = Account.objects.filter(id=pk).first()
     if detail_object:
-        list_data = Transactions.objects.filter(account_num=detail_object.account_num).all()
+        list_data = Transactions.objects.filter(
+            account_num=detail_object.account_num).all()
         print(list_data)
 
     context = {
@@ -79,6 +83,7 @@ def my_combined_view(request, pk=None):
         'list_data': list_data if list_data else None,          # For ListView
     }
     return render(request, 'transactions/account_detail.html', context)
+
 
 def django_admin_like_search(model, search_term, search_fields):
     all_queries = None
@@ -107,6 +112,7 @@ def django_admin_like_search(model, search_term, search_fields):
     # Apply the combined filters to the model queryset
     return model.objects.filter(all_queries).distinct()
 
+
 def generate_account_number(request):
     from datetime import datetime
     today = str(datetime.now()).replace(":", "")
@@ -119,11 +125,12 @@ def generate_account_number(request):
 
 
 class CreateAccountView(CreateView):
-    model=Account
+    model = Account
     fields = ["customer_phone_number", "account_num", "balance"]
 
     def form_valid(self, form):
-        customer = CustomerProfile.objects.filter(telephone=form.cleaned_data["customer_phone_number"]).first()
+        customer = CustomerProfile.objects.filter(
+            telephone=form.cleaned_data["customer_phone_number"]).first()
         print(form.cleaned_data['customer_phone_number'], customer)
         if customer:
             customer = customer.customer
@@ -131,15 +138,14 @@ class CreateAccountView(CreateView):
             self.object = form.save(commit=False)
             self.object.customer_id = Serializer.dumps(customer)["id"]
             messages.success(self.request,
-                            "Account created successfully", 
-                            f"Account created with this account number: {form.cleaned_data['account_num'] }")
+                             "Account created successfully",
+                             f"Account created with this account number: {form.cleaned_data['account_num'] }")
             return super().form_valid(form)
         except Exception as e:
             messages.error(self.request,
-                                    "Account Not Created", 
-                                    f"No user found with this Phone Number: {form.cleaned_data['customer_phone_number'] }")
+                           "Account Not Created",
+                           f"No user found with this Phone Number: {form.cleaned_data['customer_phone_number'] }")
             return super().form_invalid(form)
-
 
 
 class TransactionListView(ListView):
@@ -152,3 +158,55 @@ class TransactionListView(ListView):
         if search_account:
             return queryset.filter(account_num=search_account).all()
         return queryset
+
+
+class BillCreateView(CreateView):
+    model = BillInfo
+    form_class = BillForm
+
+    def form_valid(self, form):
+        payer = Account.objects.filter(
+            customer_phone_number=self.request.user.telephone).first()
+        payee = Account.objects.filter(
+            account_num=form.cleaned_data["payee_acc"]).first()
+        if not payer or not payee:
+            return super().form_invalid(form)
+        data = [
+            {
+                "account": payer,
+                "account_num":payer.account_num,
+                "amount": form.cleaned_data["amount"],
+                "description": "Paid Bill",
+                "type": "Withdraw",
+            },
+            {
+                "account": payee,
+                "account_num": payee.account_num,
+                "amount": form.cleaned_data["amount"],
+                "description": "Received Bill Payment",
+                "type": "Deposit",
+            }
+        ]
+
+        if form.is_valid():
+            self.object = form.save(commit=False)
+            self.object.customer_id = self.request.user.id
+            
+            # Making Transactions and Account instances
+            if payer.balance >= form.cleaned_data["amount"]:
+                payer.balance -= form.cleaned_data["amount"]
+                payee.balance += form.cleaned_data["amount"]
+                payee.save()
+                payer.save()
+                for d in data:
+                    Transactions.objects.create(**d)
+
+                return super().form_valid(form)
+            messages.error(
+                self.request, "Not enough balance to make transactions", f"You've {payer.balance} on your account.")
+            print("Not enough balance to make transactions")
+            return super().form_invalid(form)
+        else:
+            return super().form_invalid(form)
+
+
