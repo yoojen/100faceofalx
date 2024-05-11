d36db864c849ea01665b45dcc44b7372481b15af
Eugene <eugenemutuyimana15@gmail.com>
2024-05-11 17:32:29 +0200

    changed urls

--- django_projects/banking_app/profiles/urls.py
+++ django_projects/banking_app/profiles/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import (CreateUserProfileView,UserLoginView,
-                    CustomerListView, customer_profile_view,
-                    register)
-from transactions.views import UserAccountDetailView, my_combined_view
+                    CustomerListView, UserAccountDetailView, 
+                    customer_profile_view, register)
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
