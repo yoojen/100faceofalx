24abae4508fa1937779ada9c1e8e714a3e194557
Eugene <eugenemutuyimana15@gmail.com>
2024-05-18 22:04:16 +0200

    new endpoint created

--- django_projects/banking_app/profiles/urls.py
+++ django_projects/banking_app/profiles/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import (CreateUserProfileView,UserLoginView,
-                    CustomerListView, UserAccountDetailView, 
+                    CustomerListView, UserAccountDetailView,
+                    create_password,
                     customer_profile_view, register)
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(template_name="profiles/logout.html"), name='logout'),
+    path('create-password/', create_password, name="create_password"),
     path('customers/', CustomerListView.as_view(), name="customers"),
     path('create-user/', register, name="create_user"),
     path('create-user-profile/', CreateUserProfileView.as_view(), name="create_user_profile"),
