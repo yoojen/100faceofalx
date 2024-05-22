c6fd5b3bb985d51ebcde2869460eb820816191de
Eugene <eugenemutuyimana15@gmail.com>
2024-05-22 23:30:54 +0200

    added new views

--- django_projects/banking_app/api/views.py
+++ django_projects/banking_app/api/views.py
@@ -3,8 +3,11 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from api.serializers import AccountSerializer, UserSerializer, TransactionsSerializer
-from profiles.models import User
+from api.serializers import (AccountSerializer,
+                             CreateUserLeavePasswordSerializer,
+                             UserSerializer, TransactionsSerializer,
+                             PasswordSerializer, PhoneNumberSerializer)
+from profiles.models import Profile, User
 from transactions.models import Account, Transactions
 
 
@@ -22,16 +25,24 @@ class UserViewSet(ViewSet):
         user = get_object_or_404(self.queryset, pk=pk)
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def create(self, request):
+        serializer = CreateUserLeavePasswordSerializer(data = request.data)
+        if serializer.is_valid():
+            instance = serializer.save()
+            return Response({"message": f"User {instance.first_name} was created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
     def update(self, request, pk=None):
         try:
             user = self.queryset.get(pk=pk)
-            serializer = UserSerializer(user, data=request.data)
+            serializer = UserSerializer(user, data=request.data, partial=True)
 
             if serializer.is_valid():
-                print(serializer.data)
                 serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response({"message": "Updated successfully", "updated_fields": serializer.validated_data}, status=status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except User.DoesNotExist:
@@ -74,3 +85,65 @@ class UserViewSet(ViewSet):
 
         except User.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+    @action(detail=False, methods=["POST"])
+    def find_user(self, request):
+        serializer = PhoneNumberSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = User.objects.filter(telephone=serializer.validated_data["telephone"]).first()
+        if user:
+            return Response({"user_id": user.id, "message": "User found"}, status=status.HTTP_200_OK)
+        return Response({"message": "No uer found"}, status=status.HTTP_404_NOT_FOUND)
+    
+
+    @action(detail=True, methods=["POST"])
+    def set_password(self, request, pk=None):
+        serializer = PasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = User.objects.get(pk=pk)
+        if user.password:
+            return Response({"message": "Password already set"}, status=status.HTTP_400_BAD_REQUEST)
+        user.set_password(serializer.validated_data["password"])
+        user.save()
+        return Response({"message": "Password has been set"}, status=status.HTTP_202_ACCEPTED)
+    
+
+
+class UserProfileViewSet(ViewSet):
+    querset = Profile.objects.all()
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+    
+    def retrieve(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def delete(self, request, pk=None):
+        pass
+
+    @action(detail=True, methods=["GET"])
+    def user_birth(self, request):
+        """Return user based on year of birth"""
+        pass
+
+    @action(detail=True, methods=['GET'])
+    def user_age_gt(self, request):
+        """Select users with age gt requested age"""
+        pass
+
+    @action(detail=True, methods=['GET'])
+    def user_age_lt(self, request):
+        """Select users with age lt requested age"""
+        pass
+    
+    @action(detail=True, methods=['GET'])
+    def user_provice_district(self, request):
+        """Select users with age lt requested age"""
+        pass
