4c08b8487cfd664d6d81dbdae095650a36a73eda
Eugene <eugenemutuyimana15@gmail.com>
2024-05-23 23:05:53 +0200

    added new serializers

--- django_projects/banking_app/api/serializers.py
+++ django_projects/banking_app/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from profiles.models import User
+from profiles.models import Profile, User
 from transactions.models import Account, Transactions
 
 
@@ -27,12 +27,16 @@ class CreateUserLeavePasswordSerializer(serializers.ModelSerializer):
         model = User
         fields = ["telephone", "email", "first_name", "last_name"]
 
+    def validate_telephone(self, value):
+        if len(value) != 13:
+            raise ValueError("Phone number must be equal to 13")
 
 class PhoneNumberSerializer(serializers.Serializer):
     telephone = serializers.CharField(max_length=13)
     class Meta:
         model = User
         fields = ["telephone"]
+    
 
 
 class PasswordSerializer(serializers.Serializer):
@@ -40,3 +44,21 @@ class PasswordSerializer(serializers.Serializer):
     class Meta:
         model = User
         fields = ["password"]
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+    @staticmethod
+    def check_phone_number(telephone):
+        user = User.objects.filter(telephone=telephone)
+        if user:
+            return user
+        raise ValueError("No user found")
+
+    def already_exists(self, value):
+        user = Profile.objects.filter(customer=value).first()
+        if user:
+            raise ValueError("Profile already exists")
