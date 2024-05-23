b77c02de3934fa76b63385f2d6a23d89e68030b0
Eugene <eugenemutuyimana15@gmail.com>
2024-05-23 23:06:32 +0200

    added new routes

--- django_projects/banking_app/api/urls.py
+++ django_projects/banking_app/api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
-from api.views import UserViewSet
+from api.views import UserViewSet, UserProfileViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename="user")
+router.register(r'users', UserViewSet, basename="users")
+router.register(r'profiles', UserProfileViewSet, basename="profiles")
 urlpatterns = router.urls
