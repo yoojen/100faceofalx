da681496505563931c431701cca241e292c1ae36
Eugene <eugenemutuyimana15@gmail.com>
2024-05-25 20:02:54 +0200

    remaining files

--- django_projects/banking_app/api/urls.py
+++ django_projects/banking_app/api/urls.py
@@ -1,7 +1,11 @@
 from rest_framework import routers
-from api.views import UserViewSet, UserProfileViewSet
+from .users import views as UserViews
+from .user_profile import views as UserProfilesViews 
+from .accounts import views as AccountViews
+
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename="users")
-router.register(r'profiles', UserProfileViewSet, basename="profiles")
+router.register(r'users', UserViews.UserViewSet, basename="users")
+router.register(r'profiles', UserProfilesViews.UserProfileViewSet, basename="profiles")
+router.register(r'acc', AccountViews.AccountViewSet, basename="accounts")
 urlpatterns = router.urls
