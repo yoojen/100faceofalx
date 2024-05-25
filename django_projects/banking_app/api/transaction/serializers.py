54fa06a4aae2d6aeb6ad681ba91ef11f9a7218cc
Eugene <eugenemutuyimana15@gmail.com>
2024-05-25 20:02:00 +0200

    account endpoints

---
+++ django_projects/banking_app/api/transaction/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from transactions.models import Transactions
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transactions
+        fields = "__all__"
+
