5463c9ddbf3be4ea90777473ad47a9fe2338aac7
Eugene <eugenemutuyimana15@gmail.com>
2024-05-27 23:14:54 +0200

    added transaction urls

--- django_projects/banking_app/api/urls.py
+++ django_projects/banking_app/api/urls.py
@@ -2,10 +2,12 @@ from rest_framework import routers
 from .users import views as UserViews
 from .user_profile import views as UserProfilesViews 
 from .accounts import views as AccountViews
+from .transaction import views as TransactionViews
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViews.UserViewSet, basename="users")
 router.register(r'profiles', UserProfilesViews.UserProfileViewSet, basename="profiles")
-router.register(r'acc', AccountViews.AccountViewSet, basename="accounts")
+router.register(r'accounts', AccountViews.AccountViewSet, basename="accounts")
+router.register(r'transactions', TransactionViews.TransactionsViewSet, basename='transactions')
 urlpatterns = router.urls
