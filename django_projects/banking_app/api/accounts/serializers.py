d48d0c3de4e48f67b9ebee0b3dc63f2c49ff1b76
Eugene <eugenemutuyimana15@gmail.com>
2024-05-27 23:13:46 +0200

    accounts serializers

--- django_projects/banking_app/api/accounts/serializers.py
+++ django_projects/banking_app/api/accounts/serializers.py
@@ -1,8 +1,22 @@
 from transactions.models import Account
 from rest_framework import serializers
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
+
 
 class AccountSerializer(serializers.ModelSerializer):
     customer = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Account
         fields = "__all__"
+
+    def validate_customer_phone_number(self, value):
+        if not (os.environ.get('AIRTEL_1') == value[:3] or os.environ.get('AIRTEL_2')==value[:3]\
+                or os.environ.get('MTN_1') == value[:3] or os.environ.get('MTN_2')==value[:3]):
+            raise serializers.ValidationError("Phone number should begin with 078 | 072 | 073")
