11c269a5da6b04441cdd5cefa63d4020ce7cf280
Eugene <eugenemutuyimana15@gmail.com>
2024-05-29 19:10:04 +0200

    fixed errors

--- django_projects/banking_app/api/transaction/views.py
+++ django_projects/banking_app/api/transaction/views.py
@@ -54,53 +54,135 @@ class TransactionsViewSet(ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     def destroy(self, request, pk=None):
-        pass
+        try:
+            transaction = self.get_object(pk=pk)
+            transaction.delete()
+            return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response({"errors": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
     @action(detail=False, methods=["POST"], url_path="deposit", url_name="deposit")
     def deposit(self, request):
         """Process deposit transctions"""
-        pass
+        if request.data.get('type') != "Deposit":
+            return Response({"errors": "Please provide correct type"})
+        try:
+            serializer = TransactionSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            account = Account.objects.filter(
+                account_num=request.data.get('account_num')).first()
+            account.balance = float(account.balance) + \
+                float(request.data.get('amount'))
+            account.save()
+            serializer.save()
+            return Response({"message": "Recorded successfully"})
+        except Exception as e:
+            return Response({"errors": str(e)})
 
     @action(detail=False, methods=["POST"], url_path="withdraw", url_name="deposit")
     def withdraw(self, request):
         """Process withdraw transctions"""
-        pass
-
-    @action(detail=False,methods=['GET'], url_path='trans-for-acc',
-            url_name='transction_for_acc')
-    def transaction_by_account(self, request, acc_num=None):
-        """Return transaction for a passed accunt number"""
-        pass
+        if request.data.get('type') != "Withdraw":
+            return Response({"errors": "Please provide correct type"})
+        try:
+            serializer = TransactionSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            account = Account.objects.filter(
+                account_num=request.data.get('account_num')).first()
+            if account.balance >= float(request.data.get('amount')):
+                account.balance = float(account.balance) - \
+                    float(request.data.get('amount'))
+                account.save()
+                serializer.save()
+                return Response({"message": "Recorded successfully"}, status=status.HTTP_201_CREATED)
+            return Response({"erros": "Insuficient funds"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"errors": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=["GET"], url_path="trans-date", url_name="transactions_date")
     def transactions_by_date(self, request):
         """Returns transactions that occured in range of date"""
-        pass
+        res = {}
+        data = request.query_params
+        if data.keys not in ['from_date', 'to_date']:
+            return Response({"errors": "Please provide correct query params"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        try:
+            if not data.get('from_date') and data.get('to_date'):
+                res = self.queryset.filter(date_done__lte=data.get('to_date')).all()
+                serializer = TransactionSerializer(res, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            if data.get('from_date') and not data.get('to_date'):
+                res = self.queryset.filter(
+                    date_done__gte=data.get('from_date')).all()
+                serializer = TransactionSerializer(res, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            res = self.queryset.filter(date_done__gte=data.get('from_date'),
+                                    date_done__lte=data.get('to_date')).all()
+            serializer = TransactionSerializer(res, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"errors": str(e), "message": "Something went wrong, try again!"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=["GET"], url_path="trans-amt", url_name="transactions_amt")
     def transactions_by_amount(self, request):
         """Returns transaction based on amount query param"""
-        pass
+        try:
+            data = request.query_params
+            if data.keys not in ['from_amt', 'to_amt']:
+                return Response({"errors": "Please provide correct query params"},
+                                status=status.HTTP_400_BAD_REQUEST)
+            if not data.get('from_amt') and data.get('to_amt'):
+                res = self.queryset.filter(
+                    amount__lte=data.get('to_amt')).all()
+                serializer = TransactionSerializer(res, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            if data.get('from_amt') and not data.get('to_amt'):
+                res = self.queryset.filter(
+                    amount__gte=data.get('from_amt')).all()
+                serializer = TransactionSerializer(res, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            res = self.queryset.filter(amount__gte=data.get('from_amt'),
+                                       amount__lte=data.get('to_amt')).all()
+            serializer = TransactionSerializer(res, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"errors": str(e), "message": "Something went wrong, try again!"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=["POST"], url_path='pay-bills', url_name='pay_bills')
     def transaction_paybills(self, request):
         """Allow customer to pay bills"""
         pass
 
-    @action(detail=False, methods=["GET"], url_path="recent-transactions", url_name="recent_transactions")
-    def transaction_recent_paid(self, request, acc_num=None):
+    @action(detail=False, methods=["GET"], url_path="recent-trans", url_name="recent_transactions")
+    def transaction_recent_paid(self, request):
         """
-        Return recent transactions made by a user
-        This is implemented to easy payment when payment is done
-        to same user frequently
+        Return recent transactions
         """
+        serializer = TransactionSerializer(self.queryset[:10], many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-        pass
+        
 
-    @action(detail=False, methods=["GET"])
+    @action(detail=False, methods=["GET"], url_path='trans-type', url_name='transaction_by_type')
     def transactions_by_type(self, request):
         """Return transactions based on type: Deposit | Withdraw """
-        pass
+        data = request.query_params
+        if data.get('type') not in ["Deposit", "Withdraw"]:
+            return Response({"errors": "Please provide correct query params"}, 
+                            status=status.HTTP_400_BAD_REQUEST)
+        if data.get('type') == "Deposit":
+            serializer = TransactionSerializer(self.queryset.filter(
+                type=data.get('type')).all(), many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if data.get('type') == "Withdraw":
+            serializer = TransactionSerializer(
+                self.queryset.filter(type=data.get('type')).all(), many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
 
     @action(detail=False, methods=["GET"], url_path="trans-type-amt", url_name="trans_type_amt")
     def transactions_by_type_and_amount(self, request):
