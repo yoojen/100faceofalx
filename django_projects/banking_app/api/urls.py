354009005a6f2bf17f91ee2c052d8e800529bc1a
Eugene <eugenemutuyimana15@gmail.com>
2024-05-30 19:37:54 +0200

    added bills url

--- django_projects/banking_app/api/urls.py
+++ django_projects/banking_app/api/urls.py
@@ -3,6 +3,7 @@ from .users import views as UserViews
 from .user_profile import views as UserProfilesViews 
 from .accounts import views as AccountViews
 from .transaction import views as TransactionViews
+from .bills import views as BillViews
 
 
 router = routers.DefaultRouter()
@@ -10,4 +11,5 @@ router.register(r'users', UserViews.UserViewSet, basename="users")
 router.register(r'profiles', UserProfilesViews.UserProfileViewSet, basename="profiles")
 router.register(r'accounts', AccountViews.AccountViewSet, basename="accounts")
 router.register(r'transactions', TransactionViews.TransactionsViewSet, basename='transactions')
+router.register(r'bills', BillViews.BillViewSet, basename='bills')
 urlpatterns = router.urls
