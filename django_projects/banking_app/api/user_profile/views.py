222c84c6c0e17ca4b03ae0e28287d16e03f59789
Eugene <eugenemutuyimana15@gmail.com>
2024-05-30 17:37:05 +0200

    modified profile creation

--- django_projects/banking_app/api/user_profile/views.py
+++ django_projects/banking_app/api/user_profile/views.py
@@ -1,30 +1,27 @@
 from rest_framework.response import Response
-from profiles.models import Profile
-from rest_framework.viewsets import ViewSet
-from .serializers import ProfileSerializer
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import action
+from .serializers import ProfileSerializer
+from profiles.models import Profile, User
+from django.shortcuts import get_object_or_404
 from datetime import datetime
 
 class UserProfileViewSet(ViewSet):
     queryset = Profile.objects.all()
-
     def list(self, request):
         serializer = ProfileSerializer(self.queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-
         try:
-            serializer = ProfileSerializer(data=request.data)
-            user = ProfileSerializer.check_phone_number(
-                telephone=serializer.initial_data['telephone']).first()
-
-            # check profile existence
-            ProfileSerializer.already_exists(self, user.id)
-
-            serializer.initial_data["customer"] = user.id
+            data = request.data.copy()
+            telephone = data.get('telephone')
+            user = User.objects.filter(telephone=telephone).first()
+            if not user:
+                raise ValueError("No user found")
+            data["customer"] = user.id
+            serializer = ProfileSerializer(data=data)
             serializer.is_valid(raise_exception=True)
             instance = serializer.save()
             return Response({"user": instance.id, "message": "User Profile created"})
@@ -54,28 +51,36 @@ class UserProfileViewSet(ViewSet):
         try:
             profile = self.queryset.get(pk=pk)
             profile.delete()
-            return Response({"message": "User profile deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+            return Response({"message": "User profile deleted successfully"}, 
+                            status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=["GET"])
     def user_birth(self, request):
         """Return user based on year of birth"""
+        print(request.query_params.keys())
+        for k in request.query_params.keys():
+            print(request.query_params.keys())
+            if not request.query_params.get('dob') or k not in ['dob']:
+                return Response({"errors": "Please provide query"}, status=status.HTTP_400_BAD_REQUEST)
         try:
+            dob = request.query_params.get('dob')
             profiles = self.queryset.filter(dob=datetime.strptime(
-                request.GET.get('dob'), "%Y-%m-%d")).all()
+                dob, "%Y-%m-%d")).all()
             serializer = ProfileSerializer(profiles, many=True)
 
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({"errors": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"errors": str(e), "message": "Something went wrong"}, 
+                            status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['GET'])
     def user_age_gt(self, request):
         """Select users with age gt requested age"""
         try:
             profiles = self.queryset.filter(dob__gt=datetime.strptime(
-                                            request.GET.get('dob'), "%Y-%m-%d"))
+                                            request.query_params.get('dob'), "%Y-%m-%d"))
             serializer = ProfileSerializer(profiles, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
@@ -86,7 +91,7 @@ class UserProfileViewSet(ViewSet):
         """Select users with age lt requested age"""
         try:
             profiles = self.queryset.filter(dob__lt=datetime.strptime(
-                                            request.GET.get('dob'), "%Y-%m-%d"))
+                                            request.query_params.get('dob'), "%Y-%m-%d"))
             serializer = ProfileSerializer(profiles, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
