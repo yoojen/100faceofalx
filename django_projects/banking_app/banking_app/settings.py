9d63817fb0338649750a366f85c1225b8f870da2
Eugene <eugenemutuyimana15@gmail.com>
2024-05-30 19:38:17 +0200

    added middleware for getting current request

--- django_projects/banking_app/banking_app/settings.py
+++ django_projects/banking_app/banking_app/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'transactions.utils.RequestMiddleware',
 ]
 
 REST_FRAMEWORK = {
