e885466fa4a1787e12d2624f01a267a53b72c12c
Eugene <eugenemutuyimana15@gmail.com>
2024-06-01 20:22:25 +0200

    updated bills serializers

--- django_projects/banking_app/api/bills/serializers.py
+++ django_projects/banking_app/api/bills/serializers.py
@@ -7,9 +7,11 @@ class BillsSerializer(serializers.ModelSerializer):
     class Meta:
         model = BillInfo
         fields = "__all__"
+        read_only_fields = ['customer']
 
     def validate(self, data):
         if data['payee_account'] == Account.objects.filter(customer_id=data['customer']).first():
+            print("reaches here")
             raise serializers.ValidationError("You can't sent to same account")
 
         return data
