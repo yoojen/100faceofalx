06fdf0e8e2aae622387092217207c485d8845578
Eugene <eugenemutuyimana15@gmail.com>
2024-06-03 16:33:26 +0200

    validation of an account

--- django_projects/banking_app/api/transaction/serializers.py
+++ django_projects/banking_app/api/transaction/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
-from transactions.models import Transactions
+from transactions.models import Transactions, Account
 
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transactions
         fields = "__all__"
+        read_only_fields = ['account']
+
+    def validate(self, data):
+        acc_num = data.get('account_num', None)
+        account = Account.objects.filter(
+            account_num=acc_num).first()
+        if not account:
+            raise serializers.ValidationError("No associated account found")
+        data['account_id'] = account.id
+        return data
 
     
