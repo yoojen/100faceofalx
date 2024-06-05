35cec9901caa0bcaaa2327d8ac9414d9032d6018
Eugene <eugenemutuyimana15@gmail.com>
2024-06-05 17:39:24 +0200

    added new form for user profile creation

--- django_projects/banking_app/transactions/forms.py
+++ django_projects/banking_app/transactions/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import BillInfo
+from .models import Account, BillInfo
 
 class BillForm(forms.ModelForm):
     payee_acc = forms.CharField(max_length=13, required=True, label="Payee Account Number")
@@ -7,3 +7,10 @@ class BillForm(forms.ModelForm):
     class Meta:
         model = BillInfo
         fields = ["payee_acc", "payee_name", "amount"]
+
+class AccountCreationForm(forms.ModelForm):
+    phone_number = forms.CharField(max_length=13, required=True)
+
+    class Meta:
+        model = Account
+        fields = ["phone_number", "account_num", "balance"]
