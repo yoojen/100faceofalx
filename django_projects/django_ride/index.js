552c899e2544e1a0890de85dc4bb4ba68d9762ac
Eugene <eugenemutuyimana15@gmail.com>
2024-08-30 21:10:28 +0200

    'guidelines'

--- django_projects/django_ride/index.js
+++ django_projects/django_ride/index.js
@@ -7,14 +7,6 @@
   const mapObject = new MyMap(element, options.MAP_OPTIONS);
   // create map
   const map = mapObject.create();
-  
-  mapObject.onEvent({
-    event: 'click',
-    obj: map,
-    callback: function (e) {
-      alert('map clicked')
-    }
-  })
 
   const marker = mapObject.addMarker({
     latlng: [
@@ -38,29 +30,10 @@
     content: 'now clicked' 
     }
   })
-  // mapObject.removeMarker(marker)
-  // const match = mapObject.findMarker({ lat: -1.3906486179213422, lng: 29.745858907699585 });
-  
-  /*
-  let i = 0;
-  for (; i < 40; i++){
-    let marker2 = mapObject.addMarker({
-      latlng: [
-        -1.8796486179213423,
-        29.145858907699585
-      ],
-      draggable: true,
-      event: {
-        name: 'click',
-        callback: function (e) {
-          var tooltip = L.tooltip(e.latlng,
-            { content: 'Your Location' }
-          )
-          .addTo(map);
-        }
-      }
-    })
-  }*/
+  var imageUrl = 'https://maps.lib.utexas.edu/maps/historical/newark_nj_1922.jpg',
+    imageBounds = [[-1.3796486179213423, 29.746], [-1.379, 29.999]];
+  L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
   mapObject.zoom(13)
 })(window, window.MapClass);
 
