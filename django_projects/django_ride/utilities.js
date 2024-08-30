1a24f4e56c0b1b7c8ae9aee7e9d7a9aaefb30d00
Eugene <eugenemutuyimana15@gmail.com>
2024-08-30 21:49:54 +0200

    moved to new folder

--- django_projects/django_ride/utilities.js
+++
@@ -1,8 +0,0 @@
-(function (window, leaflet, utilities) {
-    utilities.icon= L.icon({
-      iconUrl: "loc.png",
-      iconSize: [40, 40],
-      iconAnchor: [0, 0]
-    });
-    window.utilites = utilities;
-})(window, L, window.utilites || (window.utilities = {}));
