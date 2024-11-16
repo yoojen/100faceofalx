c6cf3e2c87ff24ef23590acfd3c18f2c97f9c77d
Eugene <eugenemutuyimana15@gmail.com>
2024-11-16 18:12:35 +0200

    updated layout

--- nodejs_projects/inventory_mgmt/frontend/src/components/Layout/index.js
+++ nodejs_projects/inventory_mgmt/frontend/src/components/Layout/index.js
@@ -16,15 +16,15 @@ const Layout = ({ children }) => {
                 <GiHamburgerMenu className="fixed top-5 left-2 z-50 bg-white cursor-pointer h-5 w-5 hover:text-blue-500 peer"
                     onClick={() => dispatch(toggleSideNav())}
                     />
-                <div class="absolute top-6 left-7 hidden text-sm text-white bg-gray-800 px-2 z-30 md:peer-hover:block">
+                <div class="absolute top-6 left-7 hidden text-sm text-white bg-gray-800 px-2 z-40 md:peer-hover:block">
                     Toggle navigation
                 </div>
             </div>
             <div className='w-full'>
-                <div className={`${sideNav ? 'h-screen w-2/4 shadow-md bg-white p-5 space-y-32 md:block md:w-sidebar-width md:shadow-none' : 'hidden'} fixed top-0 z-20`}>
+                <div className={`${sideNav ? 'h-screen w-2/4 shadow-md bg-white p-5 space-y-32 md:block md:w-sidebar-width md:shadow-none' : 'hidden'} fixed top-0 z-30`}>
                     <SideNav />
                 </div>
-                <div className={`${sideNav ? 'w-full md:ml-[18%] md:w-navbar-width' : 'ml-0 w-full'} fixed top-0 z-20 transition-all duration-300`}>
+                <div className={`${sideNav ? 'w-full md:ml-[18%] md:w-navbar-width' : 'ml-0 w-full'} transition-all duration-300 fixed top-0 z-20`}>
                     <NavBar />
                 </div>
             </div>
