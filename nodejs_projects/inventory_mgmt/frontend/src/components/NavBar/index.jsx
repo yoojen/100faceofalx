58d039c448fb7df9ccd5b69a97b99906a98a8ff2
Eugene <eugenemutuyimana15@gmail.com>
2024-11-16 18:13:10 +0200

    added notification and profile dropdown

--- nodejs_projects/inventory_mgmt/frontend/src/components/NavBar/index.jsx
+++ nodejs_projects/inventory_mgmt/frontend/src/components/NavBar/index.jsx
@@ -1,26 +1,36 @@
 import { IoSearchOutline } from "react-icons/io5";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import DropDown from "../DropDown";
+import { useState } from "react";
 
 function NavBar() {
+  const [notificationOpen, setNofiticationOpen] = useState(false)
+  const [profileOpen, setProfileOpen] = useState(false)
 
   const searchChanged = () => {
     console.log("Typing complete!");
   }
-
+  
   const toggleNotification = () => {
-    alert("requested to toggle notification");
+    if (profileOpen) {
+      setProfileOpen(prev => !prev);
+    }
+    setNofiticationOpen((prev) =>   !prev);
   }
-
+  
   const toggleProfile = () => {
-    alert("requested to toggle profile");
+    if (notificationOpen) {
+      setNofiticationOpen(prev => !prev);
+    }
+    setProfileOpen((prev) => !prev);
   }
 
   return (
-    <div className="px-8 py-3 flex justify-between w-full bg-white">
-      <div className="relative w-full">
+    <div className="px-8 py-3 flex justify-between bg-white">
+      <div className="relative basis-3/4">
         <input
           type="text"
-          className="border w-2/3 px-7 py-2 lg:w-1/2"
+          className="border px-7 py-2 md:w-full"
           id="search"
           name="search"
           placeholder="shaka igicuruzwa cg uwakiguhaye"
@@ -29,17 +39,33 @@ function NavBar() {
         <IoSearchOutline  className="absolute top-1/4 left-2 w-5 h-5"  width={50} height={50}/>
       </div>
 
-      <div className="flex">
-        <div className="relative cursor-pointer">
-          <IoMdNotificationsOutline className="h-10 w-10" onClick={toggleNotification}/>
-          <span>
-            <small
-              className="absolute -top-3 left-1/2 bg-blue-400 text-white rounded-full p-1 font-medium">
-              3
-            </small>
-          </span>
+      <div className="flex basis-1/4 justify-end">
+        <div className="cursor-pointer">
+          <div className="relative">
+            <IoMdNotificationsOutline className="h-10 w-10" onClick={toggleNotification} />
+            <span>
+              <small
+                className="absolute -top-3 left-1/2 bg-blue-400 text-white rounded-full p-1 font-medium">
+                3
+              </small>
+            </span>
+          </div>
+            { notificationOpen && (
+              <div className="absolute z-10 top-14 right-2 w-3/4 md:w-1/4 bg-slate-300  px-2 shadow-md rounded-md content-center">
+                <DropDown type='Notification' />
+              </div>
+            )
+          }
         </div>
-        <img src="/assets/wa2024.jpg" alt="" className="rounded-full h-10 cursor-pointer" onClick={toggleProfile}/>
+        <div>
+          <img src="/assets/wa2024.jpg" alt="" className="rounded-full h-10 cursor-pointer" onClick={toggleProfile} />
+          {profileOpen && (
+              <div className="absolute z-10 top-14 right-2 w-2/4 md:w-1/4 bg-slate-300  px-4 py-4 shadow-md rounded-md content-center">
+                <DropDown type='Profile' />
+              </div>
+            )
+          }
+        </div>
       </div>
     </div>
   )
