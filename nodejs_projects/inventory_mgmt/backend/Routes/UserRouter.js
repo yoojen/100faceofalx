70e88ff052d761e462f8bf3c4a484f80de35fc8c
Eugene <eugenemutuyimana15@gmail.com>
2024-12-01 17:51:14 +0200

    changed folder structure

--- nodejs_projects/inventory_mgmt/backend/Routes/UserRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express').Router()
-const {getUsers, createUser, getUserById} = require('../Controllers/UserController');
-
-Router.get('/users', getUsers);
-Router.post('/create-user', createUser);
-Router.get('/users/:username', getUserById)
-
-module.exports = Router;
