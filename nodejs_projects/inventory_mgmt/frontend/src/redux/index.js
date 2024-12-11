2a3dc6dd57bbd3e06de60c1eb01b5c25193ccfab
Eugene <eugenemutuyimana15@gmail.com>
2024-12-11 18:09:26 +0200

    deleted unused

--- nodejs_projects/inventory_mgmt/frontend/src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import SideNavSlice from './SideNav';
-
-const store = configureStore({
-    reducer: {
-        sidenav: SideNavSlice
-    }
-})
-export default store;
